94c43469cfe2fb4d1256f706e333a995fddbb532
Ryan-Cooper <eremenion@gmail.com>
2014-12-07 11:24:40 -0600

    Add a copy of jam source so it exists through future updates

---
+++ jam/js/ai.js
@@ -0,0 +1,113 @@
+var aiOneRelDropInterval = 0;
+var doNotGiftOne = 0;
+function aiBrainOne(){
+	aiOneRelDropInterval++;
+	if(aiOneRel <= 60){
+		doNotGiftOne = 1;
+	}
+	aiCollectWoodFor();
+	if(aiWoodOne >= 50){
+		aiCollectStoneOne();
+	} else{
+		aiCollectWoodFor();
+	}
+	if(aiStoneOne >= 50){
+		if(aiWoodOne >= 50){
+			if(aiFoodOne >= 50){
+				if(aiFoodOne / 4 >= aiPopOne){
+					aiMakePopOne();
+				}
+			};
+		};
+		aiCollectFoodFor();
+	} else{
+		aiCollectWoodFor();
+	}
+	if(aiOneRelDropInterval == 300){
+		if(aiOneRel > 0){
+			aiOneRel = aiOneRel - 5;
+		}
+		aiOneRelDropInterval = 0;
+	}
+}
+
+function aiCollectWoodFor(){
+	aiWoodOne = aiWoodOne + 5;
+}
+function aiCollectFoodFor(){
+	aiFoodOne = aiFoodOne + 5;
+	aiStoneOne = aiStoneOne - 10;
+}
+function aiCollectStoneOne(){
+	aiStoneOne = aiStoneOne + 1;
+	aiWoodOne = aiWoodOne - 5;
+}
+function aiMakePopOne(){
+	aiStoneOne = aiStoneOne - 5;
+	aiWoodOne = aiWoodOne - 5;
+	aiFoodOne = aiFoodOne - 5;
+	aiPopOne = aiPopOne + 1;
+}
+function giveWood(ai){
+	if(ai == 1){
+		if(wood >= 10){
+			wood = wood - 10;
+			aiWoodOne = aiWoodOne + 10;
+			if(aiOneRel < 100){
+				aiOneRel = aiOneRel + 1;
+			}
+		}
+	}
+}
+function requestWood(ai){
+	if(ai == 1){
+		if(doNottGiftOne == 0){
+			wood = wood + 5;
+			aiWoodOne = aiWoodOne - 5;
+		}
+	}
+}
+function giveStone(ai){
+	if(ai == 1){
+		if(stone >= 10){
+			stone = stone - 10;
+			aiStoneOne = aiStoneOne + 10;
+			if(aiOneRel < 100){
+				aiOneRel = aiOneRel + 5;
+				if(aiOneRel > 100){
+					aiOneRel = 100;
+				}
+			}
+		}
+	}
+}
+function requestStone(ai){
+	if(ai == 1){
+		if(doNotGiftOne == 0){
+			stone = stone + 5;
+			aiStoneOne = aiStoneOne - 5;
+		}
+	}
+}
+function giveFood(ai){
+	if(ai == 1){
+		if(food >= 10){
+			food = food - 10;
+			aiFoodOne = aiFoodOne + 10;
+			if(aiOneRel < 100){
+				aiOneRel = aiOneRel + 10;
+				if(aiOneRel > 100){
+					aiOneRel = 100;
+				}
+			}
+		}
+	}
+}
+function requestFood(ai){
+	if(ai == 1){
+		if(doNotGiftOne == 0){
+			aiFoodOne = aiFoodOne - 5;
+			food = food + 5;
+		}
+	}
+}
---
+++ jam/js/conditions.js
@@ -0,0 +1,28 @@
+function checkLoss(){
+	if(population == 0){
+		isRunning = 0;
+		$("#gamescreen").hide();
+		$("#gameover").show();
+	}
+}
+
+function checkWin(){
+	if(hasWon == 0){
+		if(population >= 100){
+			if(wood >= 10000){
+				if(stone >= 5000){
+					if(food >= 15000){
+						$("#gamescreen").hide();
+						$("#gameWin").show();
+						isRunning = 0;
+					}
+				}
+			}
+		}
+		if(aiPopOne == 0){
+			$("#gamescreen").hide();
+			$("#gameWin").show();
+			isRunning = 0;
+		}
+	}
+}
