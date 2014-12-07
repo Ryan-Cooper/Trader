3e33ec49b495674639e7e5718712845e291fa584
Ryan-Cooper <eremenion@gmail.com>
2014-12-07 07:58:53 -0600

    Fixed ai not gaining resources when gifting

--- js/ai.js
+++ js/ai.js
@@ -24,8 +24,10 @@ function aiBrainOne(){
 		aiCollectWoodFor();
 	}
 	if(aiOneRelDropInterval == 300){
+		if(aiOneRel > 0){
+			aiOneRel = aiOneRel - 5;
+		}
 		aiOneRelDropInterval = 0;
-		aiOneRel = aiOneRel - 5;
 	}
 }
 
@@ -45,4 +47,55 @@ function aiMakePopOne(){
 	aiWoodOne = aiWoodOne - 5;
 	aiFoodOne = aiFoodOne - 5;
 	aiPopOne = aiPopOne + 1;
+}
+function giveWood(ai){
+	if(ai == 1){
+		if(wood >= 10){
+			wood = wood - 10;
+			aiWoodOne = aiWoodOne + 10;
+			aiOneRel = aiOneRel + 1;
+		}
+	}
+}
+function requestWood(ai){
+	if(ai == 1){
+		if(doNottGiftOne == 0){
+			wood = wood + 5;
+			aiWoodOne = aiWoodOne - 5;
+		}
+	}
+}
+function giveStone(ai){
+	if(ai == 1){
+		if(stone >= 10){
+			stone = stone - 10;
+			aiStoneOne = aiStoneOne + 10;
+			aiOneRel = aiOneRel + 5;
+		}
+	}
+}
+function requestStone(ai){
+	if(ai == 1){
+		if(doNotGiftOne == 0){
+			stone = stone + 5;
+			aiStoneOne = aiStoneOne - 5;
+		}
+	}
+}
+function giveFood(ai){
+	if(ai == 1){
+		if(food >= 10){
+			food = food - 10;
+			aiFoodOne = aiFoodOne + 10;
+			aiOneRel = aiOneRel + 10;
+		}
+	}
+}
+function requestFood(ai){
+	if(ai == 1){
+		if(doNotGiftOne == 0){
+			aiFoodOne = aiFoodOne - 5;
+			food = food + 5;
+		}
+	}
 }
